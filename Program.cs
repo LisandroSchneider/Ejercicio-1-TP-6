2dc402608a09f0e779d131937278c82ab21d6e0c
lisandroschneider <lisandroschneider7@gmail.com>
2022-10-13 18:16:15 -0300

    subir archivo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_n_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("  1-Precalentar horno 15 minutos");
+            Console.ReadKey();
+            Console.WriteLine(" 2-Batir margarina con azucar");
+            Console.ReadKey();
+            Console.WriteLine(" 3-Agregar huevos");
+            Console.ReadKey();
+            Console.WriteLine(" 4-Mezclar");
+            Console.ReadKey();
+            Console.WriteLine(" 5-Agregar leche y mezclar nuevamente");
+            Console.ReadKey();
+            Console.WriteLine(" 6-Hornear por 40 minutos");
+            Console.ReadKey();
+            Console.WriteLine(" 7-Decorar y comer");
+
+            Console.ReadKey();
+        }
+    }
+}
